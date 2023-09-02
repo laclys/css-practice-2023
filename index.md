7ba3755a8c85eee6ca792448fbe1c4813b266b80
Lac <laclys@126.com>
2023-09-03 00:12:22 +0800

    first blood

---
+++ index.md
@@ -0,0 +1,14 @@
+Ref: 掘金- CSS 技术揭秘与实战通关 Note
+
+
+### 社区好用的 clip-path 工具:
+
+- https://bennettfeely.com/clippy/
+
+- https://codepen.io/stoumann/pen/abZxoOM
+
+### clip-path局限性：
+
+使用 clip-path 生成图形的非常大一个问题在于，它无法作用完整的边框
+
+可以借助 filter: drop-shadow() 给 clip-path 图形添加边框或者阴影
---
+++ script.js
@@ -0,0 +1,46 @@
+const fs = require('fs')
+const path = require('path')
+
+// 指定要遍历的文件夹路径
+const folderPath = './'
+
+// 用于存储文件名和相对路径的数组
+const fileData = []
+
+// 遍历文件夹并收集文件名和相对路径
+function collectFileData(dir) {
+  const files = fs.readdirSync(dir)
+  files.forEach((file) => {
+    const filePath = path.join(dir, file)
+    const stats = fs.statSync(filePath)
+
+    if (stats.isFile() && path.extname(file) === '.html') {
+      // 如果是HTML文件，将文件名和相对路径添加到数组中
+      fileData.push({
+        fileName: file,
+        relativePath: path.relative(folderPath, filePath),
+      })
+    } else if (stats.isDirectory()) {
+      // 如果是子文件夹，递归遍历
+      collectFileData(filePath)
+    }
+  })
+}
+
+// 开始遍历
+collectFileData(folderPath)
+
+// 生成Markdown文档内容
+const markdownContent = fileData
+  .map((item) => {
+    return `* [${item.fileName}](https://laclys.github.io/css-practice-2023/${item.relativePath})`
+  })
+  .join('\n')
+
+// 指定要写入的Markdown文件路径
+const markdownFilePath = './README.md'
+
+// 将Markdown内容写入文件
+fs.writeFileSync(markdownFilePath, markdownContent, 'utf-8')
+
+console.log(`Markdown文件已生成：${markdownFilePath}`)
